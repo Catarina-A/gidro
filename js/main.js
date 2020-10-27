466231657d1376d6c5e023125cb3e9e86afe6926
Екатерина <kateryna_antoniuk@Kate.local>
2020-10-27 22:14:41 +0200

    before my personal changes

--- js/main.js
+++ js/main.js
@@ -27,7 +27,34 @@ $(function(){
     nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
   });
 
+  $('.catalog__filter-items-btn').on('click', function(){
+    $(this).toggleClass('catalog__filter-items-btn--active')
+  });
+
+  $('.filter-style').styler();
+
+  $('.filter__item-drop, .filter__extra').on('click', function(){
+    $(this).toggleClass('filter__item-drop--active');
+    $(this).next().slideToggle('0');
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 100000,
+    max: 500000,
+  });
+
+  $('.catalog__filter-btngrid').on('click', function(){
+    $(this).addClass('catalog__filter-button--active');
+    $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+    $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+  });
+
+  $('.catalog__filter-btnline').on('click', function(){
+    $(this).addClass('catalog__filter-button--active');
+    $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+    $('.product-item__wrapper').addClass('product-item__wrapper--list');
+  });
 
 
-
-});
+}); 
