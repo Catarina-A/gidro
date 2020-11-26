d5d502c050606bfbaef8275073996fbfe763fb2d
Екатерина <kateryna_antoniuk@Kate.local>
2020-11-26 16:38:22 +0200

    last commit

--- js/main.js
+++ js/main.js
@@ -3,16 +3,26 @@ $(function(){
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 969,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
 
   $('.tab').on('click', function(e){
     e.preventDefault();
 
     $($(this).siblings()).removeClass('tab--active');
-    $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+    $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
 
     $(this).addClass('tab--active');
     $($(this).attr('href')).addClass('tabs-content--active');
+
+    $('.product-slider').slick('setPosition')
   });
 
 
@@ -25,6 +35,39 @@ $(function(){
     slidesToScroll: 1,
     prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
     nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 1301,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 3,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 886,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
   });
 
   $('.catalog__filter-items-btn').on('click', function(){
@@ -63,6 +106,20 @@ $(function(){
     spacing: "7px",
     halfStar: true
   });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
+  });
+
+  $('.footer__topdrop').on('click', function(){
+    $(this).next().slideToggle();
+    $(this).toggleClass('footer__topdrop--active')
+  });
+
+  $('.aside__btn').on('click', function(){
+    $(this).next().slideToggle();
+  });
+
 
 
 }); 
